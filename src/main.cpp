12e558409c6c16fe7d9b1b6728030e4c0f523a37
Brennan Andruss <brennanandruss@gmail.com>
2025-01-16 13:41:44 -0800

    Reformatted base code

--- src/main.cpp
+++ src/main.cpp
@@ -20,83 +20,95 @@ int g_width, g_height;
    Helper function you will want all quarter
    Given a vector of shapes which has already been read from an obj file
    resize all vertices to the range [-1, 1]
- */
-void resize_obj(std::vector<tinyobj::shape_t> &shapes){
-   float minX, minY, minZ;
-   float maxX, maxY, maxZ;
-   float scaleX, scaleY, scaleZ;
-   float shiftX, shiftY, shiftZ;
-   float epsilon = 0.001;
+*/
+void resize_obj(std::vector<tinyobj::shape_t> &shapes)
+{
+	float minX, minY, minZ;
+	float maxX, maxY, maxZ;
+	float scaleX, scaleY, scaleZ;
+	float shiftX, shiftY, shiftZ;
+	float epsilon = 0.001;
 
-   minX = minY = minZ = 1.1754E+38F;
-   maxX = maxY = maxZ = -1.1754E+38F;
+	minX = minY = minZ = 1.1754E+38F;
+	maxX = maxY = maxZ = -1.1754E+38F;
 
-   //Go through all vertices to determine min and max of each dimension
-   for (size_t i = 0; i < shapes.size(); i++) {
-      for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) {
-         if(shapes[i].mesh.positions[3*v+0] < minX) minX = shapes[i].mesh.positions[3*v+0];
-         if(shapes[i].mesh.positions[3*v+0] > maxX) maxX = shapes[i].mesh.positions[3*v+0];
+	// Go through all vertices to determine min and max of each dimension
+	for (size_t i = 0; i < shapes.size(); i++) 
+	{
+		for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) 
+		{
+			if(shapes[i].mesh.positions[3*v+0] < minX) minX = shapes[i].mesh.positions[3*v+0];
+			if(shapes[i].mesh.positions[3*v+0] > maxX) maxX = shapes[i].mesh.positions[3*v+0];
 
-         if(shapes[i].mesh.positions[3*v+1] < minY) minY = shapes[i].mesh.positions[3*v+1];
-         if(shapes[i].mesh.positions[3*v+1] > maxY) maxY = shapes[i].mesh.positions[3*v+1];
+			if(shapes[i].mesh.positions[3*v+1] < minY) minY = shapes[i].mesh.positions[3*v+1];
+			if(shapes[i].mesh.positions[3*v+1] > maxY) maxY = shapes[i].mesh.positions[3*v+1];
 
-         if(shapes[i].mesh.positions[3*v+2] < minZ) minZ = shapes[i].mesh.positions[3*v+2];
-         if(shapes[i].mesh.positions[3*v+2] > maxZ) maxZ = shapes[i].mesh.positions[3*v+2];
-      }
-   }
+			if(shapes[i].mesh.positions[3*v+2] < minZ) minZ = shapes[i].mesh.positions[3*v+2];
+			if(shapes[i].mesh.positions[3*v+2] > maxZ) maxZ = shapes[i].mesh.positions[3*v+2];
+		}
+	}
 
-	//From min and max compute necessary scale and shift for each dimension
-   float maxExtent, xExtent, yExtent, zExtent;
-   xExtent = maxX-minX;
-   yExtent = maxY-minY;
-   zExtent = maxZ-minZ;
-   if (xExtent >= yExtent && xExtent >= zExtent) {
-      maxExtent = xExtent;
-   }
-   if (yExtent >= xExtent && yExtent >= zExtent) {
-      maxExtent = yExtent;
-   }
-   if (zExtent >= xExtent && zExtent >= yExtent) {
-      maxExtent = zExtent;
-   }
-   scaleX = 2.0 /maxExtent;
-   shiftX = minX + (xExtent/ 2.0);
-   scaleY = 2.0 / maxExtent;
-   shiftY = minY + (yExtent / 2.0);
-   scaleZ = 2.0/ maxExtent;
-   shiftZ = minZ + (zExtent)/2.0;
+	// From min and max compute necessary scale and shift for each dimension
+	float maxExtent, xExtent, yExtent, zExtent;
+	xExtent = maxX-minX;
+	yExtent = maxY-minY;
+	zExtent = maxZ-minZ;
+	if (xExtent >= yExtent && xExtent >= zExtent) 
+	{
+		maxExtent = xExtent;
+	}
+	if (yExtent >= xExtent && yExtent >= zExtent) 
+	{
+		maxExtent = yExtent;
+	}
+	if (zExtent >= xExtent && zExtent >= yExtent) 
+	{
+		maxExtent = zExtent;
+	}
+	scaleX = 2.0 /maxExtent;
+	shiftX = minX + (xExtent/ 2.0);
+	scaleY = 2.0 / maxExtent;
+	shiftY = minY + (yExtent / 2.0);
+	scaleZ = 2.0/ maxExtent;
+	shiftZ = minZ + (zExtent)/2.0;
 
-   //Go through all verticies shift and scale them
-   for (size_t i = 0; i < shapes.size(); i++) {
-      for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) {
-         shapes[i].mesh.positions[3*v+0] = (shapes[i].mesh.positions[3*v+0] - shiftX) * scaleX;
-         assert(shapes[i].mesh.positions[3*v+0] >= -1.0 - epsilon);
-         assert(shapes[i].mesh.positions[3*v+0] <= 1.0 + epsilon);
-         shapes[i].mesh.positions[3*v+1] = (shapes[i].mesh.positions[3*v+1] - shiftY) * scaleY;
-         assert(shapes[i].mesh.positions[3*v+1] >= -1.0 - epsilon);
-         assert(shapes[i].mesh.positions[3*v+1] <= 1.0 + epsilon);
-         shapes[i].mesh.positions[3*v+2] = (shapes[i].mesh.positions[3*v+2] - shiftZ) * scaleZ;
-         assert(shapes[i].mesh.positions[3*v+2] >= -1.0 - epsilon);
-         assert(shapes[i].mesh.positions[3*v+2] <= 1.0 + epsilon);
-      }
-   }
+	// Go through all verticies shift and scale them
+	for (size_t i = 0; i < shapes.size(); i++) 
+	{
+		for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) 
+		{
+			shapes[i].mesh.positions[3*v+0] = (shapes[i].mesh.positions[3*v+0] - shiftX) * scaleX;
+			assert(shapes[i].mesh.positions[3*v+0] >= -1.0 - epsilon);
+			assert(shapes[i].mesh.positions[3*v+0] <= 1.0 + epsilon);
+			shapes[i].mesh.positions[3*v+1] = (shapes[i].mesh.positions[3*v+1] - shiftY) * scaleY;
+			assert(shapes[i].mesh.positions[3*v+1] >= -1.0 - epsilon);
+			assert(shapes[i].mesh.positions[3*v+1] <= 1.0 + epsilon);
+			shapes[i].mesh.positions[3*v+2] = (shapes[i].mesh.positions[3*v+2] - shiftZ) * scaleZ;
+			assert(shapes[i].mesh.positions[3*v+2] >= -1.0 - epsilon);
+			assert(shapes[i].mesh.positions[3*v+2] <= 1.0 + epsilon);
+		}
+	}
 }
 
 
 int main(int argc, char **argv)
 {
-	if(argc < 3) {
-      cout << "Usage: raster meshfile imagefile" << endl;
-      return 0;
-   }
-	// OBJ filename
+	if(argc < 6) {
+		cerr << "Error: Not enough arguments provided" << endl;
+		cout << "Usage: ./raster [meshfile] [imagefile] [width] [height] [colormode]" << endl;
+		return 0;
+	}
+
+	// Process command line arguments
 	string meshName(argv[1]);
 	string imgName(argv[2]);
 
-	//set g_width and g_height appropriately!
-	g_width = g_height = 100;
+	g_width = atoi(argv[3]);
+	g_height = atoi(argv[4]);
 
-   //create an image
+	int colorMode = atoi(argv[5]);
+
+	// create an image
 	auto image = make_shared<Image>(g_width, g_height);
 
 	// triangle buffer
@@ -109,23 +121,25 @@ int main(int argc, char **argv)
 	vector<tinyobj::material_t> objMaterials; // material
 	string errStr;
 	
-   bool rc = tinyobj::LoadObj(shapes, objMaterials, errStr, meshName.c_str());
+	bool rc = tinyobj::LoadObj(shapes, objMaterials, errStr, meshName.c_str());
 	/* error checking on read */
-	if(!rc) {
+	if(!rc) 
+	{
 		cerr << errStr << endl;
-	} else {
- 		//keep this code to resize your object to be within -1 -> 1
-   	resize_obj(shapes); 
+	} 
+	else 
+	{
+ 		// keep this code to resize your object to be within -1 -> 1
+		resize_obj(shapes); 
 		posBuf = shapes[0].mesh.positions;
 		triBuf = shapes[0].mesh.indices;
 	}
 	cout << "Number of vertices: " << posBuf.size()/3 << endl;
 	cout << "Number of triangles: " << triBuf.size()/3 << endl;
 
-	//TODO add code to iterate through each triangle and rasterize it 
 	
-	//write out the image
-   image->writeToFile(imgName);
+	// write out the image
+	image->writeToFile(imgName);
 
 	return 0;
 }
